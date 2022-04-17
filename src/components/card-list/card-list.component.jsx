410bccae494e29b82c189e9f2cd9d16d706ce925
Imalka Weerasekara <imajavaweerasekara128@gmail.com>
2022-04-17 11:36:25 +0530

    converted card class component to functional component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,21 +1,18 @@
-import { Component } from "react";
 import CardContainer from "../card-container/card-container.component";
 import "./card-list.styles.css";
 
-class CardList extends Component {
-  render() {
-    console.log("render card-list!"); // output: render! twice -> component is re-rendering when calling setState() method and props are getting change!
-    console.log("props in card-list component", this.props);
-    const { monsters } = this.props;
+const CardList = (props) => {
+ 
+    const { monsters } = props;
 
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          return <CardContainer monster={monster}/>;
+          return <CardContainer monster={monster} />;
         })}
       </div>
     );
-  }
+  
 }
 
 export default CardList;
