fc47a831c65c4eabef1d60c133f65f9235819fbb
Imalka Weerasekara <imajavaweerasekara128@gmail.com>
2022-04-17 11:41:16 +0530

    applied code optimization

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,18 +1,12 @@
 import CardContainer from "../card-container/card-container.component";
 import "./card-list.styles.css";
 
-const CardList = (props) => {
- 
-    const { monsters } = props;
-
-    return (
-      <div className="card-list">
-        {monsters.map((monster) => {
-          return <CardContainer monster={monster} />;
-        })}
-      </div>
-    );
-  
-}
+const CardList = ({ monsters }) => (
+  <div className="card-list">
+    {monsters.map((monster) => {
+      return <CardContainer monster={monster} />;
+    })}
+  </div>
+);
 
 export default CardList;
